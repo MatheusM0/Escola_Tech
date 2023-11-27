54d75c8910c41305734ec06a7e563bbe8393f2a7
MatheusM0 <sergio.matheusdeto@gmail.com>
2023-11-27 17:09:44 -0300

    adição da classe genérica Funcionario

--- Entidades/Professor.java
+++ Entidades/Professor.java
@@ -1,14 +1,43 @@
 package Entidades;
 
-public class Professor {
-    private String nome;
-    private int idade;
-    private String curso;
+public class Professor extends Funcionario {
+    protected String curso;
 
 
-    public Professor(String nome, int idade, String curso){
-        this.nome = nome;
-        this.idade = idade;
+    public Professor(String nome, int idade, int matricula, String curso){
+        super(nome, idade, matricula);
         this.curso = curso;
     }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+    public String getCurso() {
+        return curso;
+    }
+
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    
 }
