1a0d46178fb6575fbaf03507c426bd4217c47d4b
MatheusM0 <sergio.matheusdeto@gmail.com>
2023-11-27 17:01:03 -0300

    Adição de atributos e construtor à classe Professor

--- Entidades/Professor.java
+++ Entidades/Professor.java
@@ -3,6 +3,12 @@ package Entidades;
 public class Professor {
     private String nome;
     private int idade;
-    private 
+    private String curso;
 
+
+    public Professor(String nome, int idade, String curso){
+        this.nome = nome;
+        this.idade = idade;
+        this.curso = curso;
+    }
 }
