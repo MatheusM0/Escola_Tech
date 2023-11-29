714ffd1236f9fdfa69d18924a60c4de19f4743ee
Iago Mendes <iagommendes@gmail.com>
2023-11-29 16:09:09 -0300

    new: creating new school class

---
+++ Entidades/Escola.java
@@ -0,0 +1,29 @@
+package Entidades;
+
+public class Escola {
+
+    
+    public void realizarChamada() {
+        System.out.println("Realizando chamada na escola");
+    }
+
+    public void chamarMinistrarAula(Professor professor) {
+        professor.ministrarAula();
+    }
+
+    public void organizarEvento() {
+        System.out.println("Organizando evento na escola");
+    }
+
+    public static void main(String[] args) {
+        
+        Escola escola = new Escola();
+        escola.realizarChamada();
+
+        escola.organizarEvento();
+
+        Professor professor = new Professor("João", 35, 12345, "Matemática");
+
+        escola.chamarMinistrarAula(professor);
+    }
+}
